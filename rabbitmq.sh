0cdf13fb4a25b74513e79512c8011bac9d7a3021
charitha <charitha.harinath@gmail.com>
2025-07-27 12:36:32 +0530

    Shell Roboshop 2

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -8,27 +8,27 @@ check_root
 echo "Please enter rabbitmq password"
 read -s RABBITMQ_PASSWORD
 
-cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>LOG_FILE
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
 VALIDATE $? "Creating rabbitmq repo"
 
-dnf install rabbitmq-server -y  &>>LOG_FILE
+dnf install rabbitmq-server -y  &>>$LOG_FILE
 VALIDATE $? "Installing rabbitmq server"
 
-systemctl enable rabbitmq-server   &>>LOG_FILE
+systemctl enable rabbitmq-server   &>>$LOG_FILE
 VALIDATE $? "Enabling rabbitmq server"
 
-systemctl start rabbitmq-server  &>>LOG_FILE
+systemctl start rabbitmq-server  &>>$LOG_FILE
 VALIDATE $? "Starting rabbitmq server"
 id roboshop
 if [ $? -ne 0 ]
     then
-        rabbitmqctl add_user roboshop $RABBITMQ_PASSWORD  &>>LOG_FILE
+        rabbitmqctl add_user roboshop $RABBITMQ_PASSWORD  &>>$LOG_FILE
         VALIDATE $? "Adding rabbitmq user"
     else
         echo -e "roboshop user is already created.. $Y SKIPPING $N" | tee -a $LOG_FILE
 fi
 
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>LOG_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>$LOG_FILE
 VALIDATE $? "Permission set"
 
 print_time
--- shipping.sh
+++ shipping.sh
@@ -11,21 +11,21 @@ app_setup
 maven_setup
 systemd_setup
 
-dnf install mysql -y  &>>LOG_FILE
+dnf install mysql -y  &>>$LOG_FILE
 VALIDATE $? "Installing mysql"
 
 mysql -h mysql.charitha.site -u root -p$SHIPPING_PASSWORD -e 'use cities' &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    mysql -h mysql.charitha.site -uroot -p$SHIPPING_PASSWORD < /app/db/schema.sql &>>LOG_FILE
-    mysql -h mysql.charitha.site -uroot -p$SHIPPING_PASSWORD < /app/db/app-user.sql  &>>LOG_FILE
-    mysql -h mysql.charitha.site -uroot -p$SHIPPING_PASSWORD < /app/db/master-data.sql &>>LOG_FILE
+    mysql -h mysql.charitha.site -uroot -p$SHIPPING_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h mysql.charitha.site -uroot -p$SHIPPING_PASSWORD < /app/db/app-user.sql  &>>$LOG_FILE
+    mysql -h mysql.charitha.site -uroot -p$SHIPPING_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
     VALIDATE $? "Data is loading into db"
 else
     echo -e "Data is already loaded into MYSQL... $Y SKIPPING $N"
 fi
 
-systemctl restart shipping &>>LOG_FILE
+systemctl restart shipping &>>$LOG_FILE
 VALIDATE $? "Restart shipping"
 
 print_time
